1fe1236effafbf0db08b5a700922dc2af6b0d24e
nedostupno <kabanov.md@gmail.com>
2022-03-01 18:14:30 +0300

    Изменены названия полей у ManagerConfig.Trace

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,11 +6,11 @@ import (
 
 type ManagerConfig struct {
 	Trace struct {
-		DEFAULT_PORT        int `yaml:"default_port"`
-		DEFAULT_MAX_HOPS    int `yaml:"default_max_hops"`
-		DEFAULT_TIMEOUT_MS  int `yaml:"default_timeout_ms"`
-		DEFAULT_RETRIES     int `yaml:"default_retries"`
-		DEFAULT_PACKET_SIZE int `yaml:"default_packet_size"`
+		Port        int `yaml:"port"`
+		Max_hops    int `yaml:"max_hops"`
+		Timeout_ms  int `yaml:"timeout_ms"`
+		Retries     int `yaml:"retries"`
+		Packet_size int `yaml:"packet_size"`
 	} `yaml:"trace"`
 	Jwt struct {
 		SecretKeyForAccessToken  string `yaml:"secret_key_for_access_token"`
