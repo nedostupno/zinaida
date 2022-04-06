63987ee7f4ca7282f3126062e713deea026f2c44
nedostupno <kabanov.md@gmail.com>
2022-04-06 14:29:58 +0300

    Удалены некоторые структуры из пакета models

--- internal/models/model.go
+++ internal/models/model.go
@@ -1,19 +1,7 @@
 package models
 
-import "github.com/golang-jwt/jwt"
-
-type Token struct {
-	UserId uint
-	jwt.StandardClaims
-}
-
 type RefreshToken struct {
 	Token string `json:"token"`
-}
-
-type Account struct {
-	Password string `json:"password"`
-	Login    string `json:"login"`
 }
 
 type Credential struct {
@@ -26,21 +14,6 @@ type NodeAgent struct {
 	Ip          string `json:"ip"`
 	Domain      string `json:"domain"`
 	Unreachable int    `json:"unreachable"`
-}
-
-type CPU struct {
-	Model   string `json:"model,omitempty"`
-	Cpu_s   string `json:"cpu___s,omitempty"`
-	Min_MHz string `json:"min___m_hz,omitempty"`
-	Max_MHz string `json:"max___m_hz,omitempty"`
-}
-
-type TopProc struct {
-	First  string `json:"first,omitempty"`
-	Second string `json:"second,omitempty"`
-	Third  string `json:"third,omitempty"`
-	Fourth string `json:"fourth,omitempty"`
-	Fifth  string `json:"fifth,omitempty"`
 }
 
 type User struct {
